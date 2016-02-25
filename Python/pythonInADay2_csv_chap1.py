6fc130370aaa0d88c718dd63a9e7a3fb302d9ca2
James Clavelli <jclavelli89@gmail.com>
2016-02-24 22:13:36 -0500

    Added python course 37/76 files

---
+++ Python/pythonInADay2_csv_chap1.py
@@ -0,0 +1,28 @@
+# CSV - Using Python to make a spread sheet
+
+import os, csv
+
+# The path to the script
+currentPath = os.path.dirname(os.path.abspath("__file__"))
+
+# Make the spreadsheet path
+outputCsv = currentPath + '/spreadsheet.csv'
+
+# Open the file
+csvFile = open(outputCsv, "wb")
+
+# Create writer object
+writer = csv.writer(csvFile, delimiter=',')
+
+# Data to go in csv
+row_1 = [1, "Row 1", 123]
+row_2 = [2, "Row 2", 456]
+row_3 = [3, "Row 3", 789]
+
+# All rows
+
+rows = [row_1, row_2, row_3]
+
+# Loop rows and write each
+for row in rows:
+    writer.writerow(row)
