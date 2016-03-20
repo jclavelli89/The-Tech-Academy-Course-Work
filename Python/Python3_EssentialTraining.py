14ae4230e77d84e0b1c523bcc35e67d06e14fb8b
James Clavelli <jclavelli89@gmail.com>
2016-03-20 18:22:33 -0400

    Completed more assignments

--- Python/Python3_EssentialTraining.py
+++ Python/Python3_EssentialTraining.py
@@ -1950,3 +1950,399 @@
 #        print(dict(row))
 
 # if __name__ == "__main__": main()
+
+##
+## Creating, retrieving, updating and deleting records
+##
+
+# import sqlite3
+#
+# def insert(db, row):
+#     db.execute('insert into test (t1, i1) values (?, ?)', (row['t1'], row['i1']))
+#     db.commit()
+#
+# def retrieve(db, t1):
+#     cursor = db.execute('select * from test where t1 = ?', (t1,))
+#     return cursor.fetchone()
+#
+# def update(db, row):
+#     db.execute('update test set i1 = ? where t1 = ?', (row['i1'], row['t1']))
+#     db.commit()
+#
+# def delete(db, t1):
+#     db.execute('delete from test where t1 = ?', (t1,))
+#     db.commit()
+#
+# def disp_rows(db):
+#     cursor = db.execute('select * from test order by t1')
+#     for row in cursor:
+#         print('  {}: {}'.format(row['t1'], row['i1']))
+#
+# def main():
+#     db = sqlite3.connect('test.db')
+#     db.row_factory = sqlite3.Row
+#     print('Create table test')
+#     db.execute('drop table if exists test')
+#     db.execute('create table test ( t1 text, i1 int )')
+#
+#     print('Create rows')
+#     insert(db, dict(t1 = 'one', i1 = 1))
+#     insert(db, dict(t1 = 'two', i1 = 2))
+#     insert(db, dict(t1 = 'three', i1 = 3))
+#     insert(db, dict(t1 = 'four', i1 = 4))
+#     disp_rows(db)
+#
+#     print('Retrieve rows')
+#     print(dict(retrieve(db, 'one')), dict(retrieve(db, 'two')))
+#
+#     print('Update rows')
+#     update(db, dict(t1 = 'one', i1 = 101))
+#     update(db, dict(t1 = 'three', i1 = 103))
+#     disp_rows(db)
+#
+#     print('Delete rows')
+#     delete(db, 'one')
+#     delete(db, 'three')
+#     disp_rows(db)
+#
+# if __name__ == "__main__": main()
+
+##
+## Creating a database object
+##
+
+# import sqlite3
+#
+# class database:
+#     def __init__(self, **kwargs):
+#         self.filename = kwargs.get('filename')
+#         self.table = kwargs.get('table', 'test')
+#
+#     def sql_do(self, sql, *params):
+#         self._db.execute(sql, params)
+#         self._db.commit()
+#
+#     def insert(self, row):
+#         self._db.execute('insert into {} (t1, i1) values (?, ?)'.format(self._table), (row['t1'], row['i1']))
+#         self._db.commit()
+#
+#     def retrieve(self, key):
+#         cursor = self._db.execute('select * from {} where t1 = ?'.format(self._table), (key,))
+#         return dict(cursor.fetchone())
+#
+#     def update(self, row):
+#         self._db.execute(
+#             'update {} set i1 = ? where t1 = ?'.format(self._table),
+#             (row['i1'], row['t1']))
+#         self._db.commit()
+#
+#     def delete(self, key):
+#         self._db.execute('delete from {} where t1 = ?'.format(self._table), (key,))
+#         self._db.commit()
+#
+#     def disp_rows(self):
+#         cursor = self._db.execute('select * from {} order by t1'.format(self._table))
+#         for row in cursor:
+#             print('  {}: {}'.format(row['t1'], row['i1']))
+#
+#     def __iter__(self):
+#         cursor = self._db.execute('select * from {} order by t1'.format(self._table))
+#         for row in cursor:
+#             yield dict(row)
+#
+#     @property
+#     def filename(self): return self._filename
+#
+#     @filename.setter
+#     def filename(self, fn):
+#         self._filename = fn
+#         self._db = sqlite3.connect(fn)
+#         self._db.row_factory = sqlite3.Row
+#
+#     @filename.deleter
+#     def filename(self): self.close()
+#
+#     @property
+#     def table(self): return self._table
+#     @table.setter
+#     def table(self, t): self._table = t
+#     @table.deleter
+#     def table(self): self._table = 'test'
+#
+#     def close(self):
+#             self._db.close()
+#             del self._filename
+#
+# def main():
+#     db = database(filename = 'test.db', table = 'test') # Initialize database
+#
+#     print('Create table test')
+#     db.sql_do('drop table if exists test')
+#     db.sql_do('create table test ( t1 text, i1 int )')
+#
+#     print('Create rows')
+#     db.insert(dict(t1 = 'one', i1 = 1))
+#     db.insert(dict(t1 = 'two', i1 = 2))
+#     db.insert(dict(t1 = 'three', i1 = 3))
+#     db.insert(dict(t1 = 'four', i1 = 4))
+#     for row in db: print(row)
+#
+#     print('Retrieve rows')
+#     print(db.retrieve('one'), db.retrieve('two'))
+#
+#     print('Update rows')
+#     db.update(dict(t1 = 'one', i1 = 101))
+#     db.update(dict(t1 = 'three', i1 = 103))
+#     for row in db: print(row)
+#
+#     print('Delete rows')
+#     db.delete('one')
+#     db.delete('three')
+#     for row in db: print(row)
+#
+# if __name__ == "__main__": main()
+
+##
+## 17. Modules
+##
+
+##
+## Using standard library modules
+##
+
+# Modules for adding functionality to your programs
+# Go through list to get familiar with list
+
+# import sys
+#
+# def main():
+#     print('Python version {}.{}.{}'.format(*sys.version_info))
+#     print(sys.platform)
+#     print()
+#
+#     # import os # Python allows you to import modules selectively within function
+#     # print(os.name) # Gets the current type of os
+#     # print(os.getenv('PATH')) # Gets the path of the ox
+#     # print(os.getcwd()) # CWD = Current working directory
+#     # print(os.urandom(25))
+#
+#     # import urllib.request
+#     # page = urllib.request.urlopen('http://bw.org/')
+#     # # print(page) # gives the object for that webpage
+#     # for line in page: print(str(line, encoding = 'UTF_8'), end = '')
+#
+#     import random
+#     # print(random.randint(1, 1000))
+#
+#     # x = list(range(25))
+#     # print(x)
+#     # random.shuffle(x)
+#     # print(x)
+#
+#     import datetime
+#     now = datetime.datetime.now()
+#     print(now)
+#     print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+#
+#
+#
+# if __name__ == "__main__": main()
+
+##
+## Finding third-party modules    #http://www.lynda.com/Python-tutorials/Finding-third-party-modules/62226/71013-4.html
+##
+
+# PyPI is http://pypi.python.org/pypi  PyPI = Python Package Index
+# All PyPI modules on site have the same setup instructions
+
+# import bitstring
+#
+# def main():
+#     a = bitstring.BitString(bin = '01010101')
+#     print(a.hex, a.bin, a.uint)
+#
+# if __name__ == "__main__": main()
+
+##
+## Creating your own module # http://www.lynda.com/Python-tutorials/Creating-module/62226/71014-4.html
+##
+
+# When creating your own module, make sure not to have any code outside of a class or function, which is a good practice anyways
+
+# import sys
+# import time
+
+# __version__ = "1.1.0"
+
+# class numwords():
+#     """
+#         return a number as words,
+#         e.g., 42 becomes "forty-two"
+#     """
+#     _words = {
+#         'ones': (
+#             'oh', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+#         ), 'tens': (
+#             '', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+#         ), 'teens': (
+#             'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' 
+#         ), 'quarters': (
+#             'o\'clock', 'quarter', 'half'
+#         ), 'range': {
+#             'hundred': 'hundred'
+#         }, 'misc': {
+#             'minus': 'minus'
+#         }
+#     }
+#     _oor = 'OOR'    # Out Of Range
+
+#     def __init__(self, n):
+#         self.__number = n;
+
+#     def numwords(self, num = None):
+#         "Return the number as words"
+#         n = self.__number if num is None else num
+#         s = ''
+#         if n < 0:           # negative numbers
+#             s += self._words['misc']['minus'] + ' '
+#             n = abs(n)
+#         if n < 10:          # single-digit numbers
+#             s += self._words['ones'][n]  
+#         elif n < 20:        # teens
+#             s += self._words['teens'][n - 10]
+#         elif n < 100:       # tens
+#             m = n % 10
+#             t = n // 10
+#             s += self._words['tens'][t]
+#             if m: s += '-' + numwords(m).numwords()    # recurse for remainder
+#         elif n < 1000:      # hundreds
+#             m = n % 100
+#             t = n // 100
+#             s += self._words['ones'][t] + ' ' + self._words['range']['hundred']
+#             if m: s += ' ' + numwords(m).numwords()    # recurse for remainder
+#         else:
+#             s += self._oor
+#         return s
+
+#     def number(self):
+#         "Return the number as a number"
+#         return str(self.__number);
+
+# class saytime(numwords):
+#     """
+#        return the time (from two parameters) as words,
+#        e.g., fourteen til noon, quarter past one, etc.
+#     """
+#
+#     _specials = {
+#         'noon': 'noon',
+#         'midnight': 'midnight',
+#         'til': 'til',
+#         'past': 'past'
+#     }
+
+#     def __init__(self, h, m):
+#         self._hour = abs(int(h))
+#         self._min = abs(int(m))
+
+#     def words(self):
+#         h = self._hour
+#         m = self._min
+        
+#         if h > 23: return self._oor     # OOR errors
+#         if m > 59: return self._oor
+
+#         sign = self._specials['past']        
+#         if self._min > 30:
+#             sign = self._specials['til']
+#             h += 1
+#             m = 60 - m
+#         if h > 23: h -= 24
+#         elif h > 12: h -= 12
+
+        # hword is the hours word)
+#         if h is 0: hword = self._specials['midnight']
+#         elif h is 12: hword = self._specials['noon']
+#         else: hword = self.numwords(h)
+
+#         if m is 0:
+#             if h in (0, 12): return hword   # for noon and midnight
+#             else: return "{} {}".format(self.numwords(h), self._words['quarters'][m])
+#         if m % 15 is 0:
+#             return "{} {} {}".format(self._words['quarters'][m // 15], sign, hword) 
+#         return "{} {} {}".format(self.numwords(m), sign, hword) 
+
+#     def digits(self):
+#         "return the traditionl time, e.g., 13:42"
+#         return "{:02}:{:02}".format(self._hour, self._min)
+
+# class saytime_t(saytime):   # wrapper for saytime to use time object
+#     """
+#         return the time (from a time object) as words
+#         e.g., fourteen til noon
+#     """
+#     def __init__(self, t):
+#         self._hour = t.tm_hour
+#         self._min = t.tm_min
+
+# def main():
+#     if len(sys.argv) > 1:
+#         if sys.argv[1] == 'test':
+#             test()
+#         else:
+#             try: print(saytime(*(sys.argv[1].split(':'))).words())
+#             except TypeError: print("Invalid time ({})".format(sys.argv[1]))
+#     else:
+#         print(saytime_t(time.localtime()).words())
+
+# def test():
+#     print("\nnumbers test:")
+#     list = (
+#         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 19, 20, 30, 
+#         50, 51, 52, 55, 59, 99, 100, 101, 112, 900, 999, 1000 
+#     )
+#     for l in list:
+#         print(l, numwords(l).numwords())
+
+#     print("\ntime test:")
+#     list = (
+#         (0, 0), (0, 1), (11, 0), (12, 0), (13, 0), (12, 29), (12, 30),
+#         (12, 31), (12, 15), (12, 30), (12, 45), (11, 59), (23, 15), 
+#         (23, 59), (12, 59), (13, 59), (1, 60), (24, 0)
+#     )
+#     for l in list:
+#         print(saytime(*l).digits(), saytime(*l).words())
+
+#     print("\nlocal time is " + saytime_t(time.localtime()).words())
+
+# if __name__ == "__main__": main()
+
+##
+## 18. Debugging 
+##
+
+##
+## 18. Debugging
+##
+
+##
+## Dealing with syntax errors
+##
+
+# Need to have patience
+
+##
+## Dealing with runtime errors
+##
+
+# Follow the execution path
+
+##
+## Dealing with Logical errors 
+##
+
+# Patience and perserverence is key here. Understand the code logically, read through it carefully and look for what is out of place. 
+
+##
+## Using unit tests:
+##
