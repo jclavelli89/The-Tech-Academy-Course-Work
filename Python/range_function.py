fe893015931a9020cecf087bf28376a2117270f1
James Clavelli <jclavelli89@gmail.com>
2016-03-22 20:22:27 -0400

    Added file

---
+++ Python/range_function.py
@@ -0,0 +1,39 @@
+'''
+1. Start IDLE and use the Python range() function with one parameter to display the
+   following:
+   
+   0
+   1
+   2
+   3
+'''
+
+# my_range = range(4)
+# for i in my_range:
+#         print(i)
+
+'''
+2. Use the Python range() function with 3 parameters to display the following:
+
+   3
+   2
+   1
+   0
+'''
+
+# my_range = range(3, -1, -1)
+# for i in my_range:
+#     print(i)
+
+'''
+3. Use the Python range() function with 3 parameters to display the following:
+
+   8
+   6
+   4  
+   2
+'''
+
+# my_range = range(8, 1, -2)
+# for i in my_range:
+#    print(i)
