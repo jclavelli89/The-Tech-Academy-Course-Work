75bbe720f9a05245b1a98cd7d28ca54e5971c103
James Clavelli <jclavelli89@gmail.com>
2016-04-13 19:42:35 -0400

    Added Files

--- Python/Python3_Tkinter_Lynda.py
+++ Python/Python3_Tkinter_Lynda.py
@@ -700,12 +700,305 @@
 ## Building cascading menus 
 ##
 
+# Dropdown menus are universally seen in programs with gui's
+# Organizing and presenting actions to the user
+# Don't let menus grow too long or become too deeply nested
+# Always test drive guis 
+
+# >>> from tkinter import *
+# >>> root = Tk()
+# >>> root.option_add('*tearOff', False)
+# >>> menubar = Menu(root)
+# >>> root.config(menu = menubar)
+# >>> file = Menu(menubar)
+# >>> edit = Menu(menubar)
+# >>> help_ = Menu(menubar)
+# >>> menubar.add_cascade(menu = file, label = 'File')
+# >>> menubar.add_cascade(menu = edit, label = 'Edit')
+# >>> menubar.add_cascade(menu = help_, label = 'Help')
+# >>> file.add_command(label = 'New', command = lambda: print('New File'))
+# >>> New File
+# New File
+# >>> file.add_separator()
+# >>> file.add_command(label = 'Open...', command = lambda: print('Opening File...'))
+# >>> file.add_command(label = 'Save', command = lambda: print('Save File'))
+# >>> file.entryconfig('New', accelerator = 'Ctrl + N')
+# >>> logo = PhotoImage(file = 'C:\\Users\\James Clavelli\\Desktop\\Ex_Files_Python_Tkinter\\Ex_Files_Python_Tkinter\\Exercise Files\\Ch05\\python_logo.gif').subsample(10, 10)
+# >>> file.entryconfig('Open...', image = logo, compound = 'left')
+# >>> Opening File...
+
+# >>> file.entryconfig('Open...', state = 'disabled')
+# >>> file.delete('Save')
+# >>> save = Menu(file)
+# >>> file.add_cascade(menu = save, label = 'Save')
+# >>> save.add_command(label = 'Save As', command = lambda: print('Saving As...'))
+# >>> save.add_command(label = 'Save All', command = lambda: print('Saving All...'))
+# >>> Saving As...
+
+# >>> choice = IntVar()
+# >>> edit.add_radiobutton(label = 'One', variable = choice, value = 1)
+# >>> edit.add_radiobutton(label = 'Two', variable = choice, value = 2)
+# >>> edit.add_radiobutton(label = 'Three', variable = choice, value = 3)
+# >>> file.post(400, 300)
+
+##
+## Drawing a basic line on the Canvas
+##
+
+# Space on which to draw shapes, images and other widgits
+# Basic drawing space/diagram, to building custom widgit with look and feel
+
+# >>> from tkinter import *
+# >>> root = Tk()
+# >>> canvas = Canvas(root)
+# >>> canvas.pack()
+# >>> canvas.config(width = 640, height = 480)
+# >>> line = canvas.create_line(160, 360, 480, 120, fill = 'blue', width = 5)
+# >>> canvas.itemconfigure(line, fill = 'green')
+# >>> canvas.coords(line)
+# [160.0, 360.0, 480.0, 120.0]
+# >>> canvas.coords(line, 0, 0, 320,  240, 640, 0)
+# []
+# >>> canvas.itemconfigure(line, smooth = True)
+# >>> canvas.itemconfigure(line, splinesteps = 5)
+# >>> canvas.itemconfigure(line, splinesteps = 100)
+# >>> canvas.itemconfigure(line, splinesteps = 1000)
+# >>> canvas.itemconfigure(line, splinesteps = 10000)
+
+##
+## Drawing complex shapes on the Canvas
+##
+
+# Draw different shapes to the canvas, more complex then justl ines. 
+# Canvas acts as geometry manager
+
+# rect = canvas.create_rectangle(160, 120, 480, 360)
+# canvas.itemconfigure(rect, fill = 'yellow')
+# oval = canvas.create_oval(160, 120, 480, 360)
+# arc = canvas.create_arc(160, 1, 480, 240)
+# canvas.itemconfigure(arc, start = 0, extent = 180, fill = 'green')
+# poly = canvas.create_polygon(160, 360, 320, 480, 480, 360, fill = 'blue')
+# text = canvas.create_text(320, 240, text = 'Python', font = ('Courier', 32, 'bold'))
+
+# logo = PhotoImage(file = 'python_logo.gif') # Change path as needed
+# image = canvas.create_image(320, 240, image = logo)
+
+# canvas.lift(text)
+# canvas.lower(image)
+# canvas.lower(image, text)
+
+# button = Button(canvas, text = 'Click Me')
+# canvas.create_window(320, 60, window = button)
+
+# canvas.itemconfigure(rect, tags = ('shape'))
+# canvas.itemconfigure(oval, tags = ('shape', 'round'))
+# canvas.itemconfigure('shape', fill = 'grey')
+# print(canvas.gettags(oval))
+
+# root.mainloop()
+
+##
+## Attaching scroll bars to widgets 
+##
+
+# Scroll-Enabled Widgits:
+# X-Scroll text, canvas, treeview, entry (x only), spinbox(x only), combobox(x only)
+# y-Scroll text, canvas, treeview 
+
+# From the course
+
+# >>> from tkinter import *
+# >>> from tkinter import ttk
+# >>> root = Tk()
+# >>> text = Text(root, width = 40, height = 10, wrap = 'word')
+# >>> text.grid(row = 0, column = 0)
+# >>> scrollbar = ttk.Scrollbar(root, orient = VERTICAL, command = text.yview)
+# >>> scrollbar.grid(row = 0, column = 1, sticky = 'ns')
+# >>> text.config(yscrollcommand = scrollbar.set)
+
+# Pre loaded from file
+
+# from tkinter import *
+# from tkinter import ttk        
+    
+# root = Tk()
+
+# canvas = Canvas(root, scrollregion = (0, 0, 640, 480), bg = 'white')
+# xscroll = ttk.Scrollbar(root, orient = HORIZONTAL, command = canvas.xview)
+# yscroll = ttk.Scrollbar(root, orient = VERTICAL, command = canvas.yview)
+# canvas.config(xscrollcommand = xscroll.set, yscrollcommand = yscroll.set)
+
+# canvas.grid(row = 1, column = 0)
+# xscroll.grid(row = 2, column = 0, sticky = 'ew')
+# yscroll.grid(row = 1, column = 1, sticky = 'ns')
+
+# def canvas_click(event):
+#     x = canvas.canvasx(event.x)
+#     y = canvas.canvasy(event.y)
+#     canvas.create_oval((x-5, y-5, x+5, y+5), fill = 'green')
+
+# canvas.bind('<1>', canvas_click)
+
+# root.mainloop()
+
+##
+## Configuring Widgit Styles  
+##
 
 
+# 10 Widgit states:
+# active, disables, focus, pressed, selected, background, readonly, alternate
+# invalid, hover. more info at: www.tkdocs.com/widgets/index.html
+
+# When selecting themes:
+# T + widget name, i.e., TButton, TFrame, etc.
+
+# from tkinter import *
+# from tkinter import ttk        
+    
+# root = Tk()
+
+# button1 = ttk.Button(root, text = 'Button 1')
+# button2 = ttk.Button(root, text = 'Button 2')      
+# button1.pack()
+# button2.pack()
+
+# style = ttk.Style()
+
+# print(style.theme_names())
+# print(style.theme_use())
+# style.theme_use('classic')
+# style.theme_use('vista')
+
+# print(button1.winfo_class())
+# style.configure('TButton', foreground = 'blue')
+# style.configure('Alarm.TButton', foreground = 'orange',
+                font = ('Arial', 24, 'bold'))
+# button2.configure(style = 'Alarm.TButton')
+# style.map('Alarm.TButton', foreground = [('pressed', 'pink'),
+                                         ('disabled', 'grey')])
+# button2.state(['disabled'])
+
+# print(style.layout('TButton'))
+# print(style.element_options('Button.label'))
+# print(style.lookup('TButton', 'foreground'))
+
+##
+## Prompting users with the Messagebox and dialogs 
+##
+
+# Pop up message for problems
+# Message box methods
+
+# Three variations of the message box:
+# showinfo()
+# showwarning()
+# showerror()
+# askyesno()
+# askokcancel()
+# askretrycancel()
+# askyesnocancel()
+# askquestion()
+
+# Filedialog Types
+
+# askopenfile(mode)
+# askopenfiles(mode)
+# askopenfilename()
+# askopenfilenames()
+
+# >>> from tkinter import messagebox
+# >>> messagebox.showinfo(title = 'A Friendly Message', message = 'Hello, Tkinter!')
+# 'ok'
+# >>> messagebox.askyesno(title = "Hungry?", message = "Do you want SPAM?")
+# True
+# >>> from tkinter import filedialog
+# >>> filedialog.askopenfile()
+# >>> filename = filedialog.askopenfile()
+# >>> filename
+# <_io.TextIOWrapper name='C:/Users/James Clavelli/Documents/GitHub/The-Tech-Academy-Course-Work/Python/python_databases_5.py' mode='r' encoding='cp1252'>
+# >>> from tkinter import colorchooser
+# >>> colorchooser.askcolor(initialcolor = '#FFFFFF')
+# ((0.0, 128.5, 255.99609375), '#0080ff')
+
+##
+## 6. Geometry Management 
+##
+
+##
+## Using the pack gemetry manager
+##
+
+# Pros
+# pack() is the simplest to use
+# Simplest to use, use when a widget is expanded to fill its entire parent
+# Use to stack multiple widgits vertically or horizontally
+
+# Cons
+# Limited capability for complex layouts
+
+# from tkinter import *
+# from tkinter import ttk        
+    
+# root = Tk()
+
+# label = ttk.Label(root, text = "Hello, Tkinter!",
+#           background = 'yellow')
+# label.pack(side = LEFT, anchor = 'nw')
+# print(label)
+
+# ttk.Label(root, text = "Hello, Tkinter!",
+#           background = 'blue').pack(side = LEFT, padx = 10, pady = 10)
+# ttk.Label(root, text = "Hello, Tkinter!",
+#           background = 'green').pack(side = LEFT, ipadx = 10, ipady = 10) 
 
 
+# for widget in root.pack_slaves():
+#     widget.pack_configure(fill = BOTH, expand = True)
+#     print(widget.pack_info()) # prints dictionaries with values related to packmanager
 
+# label.pack_forget()
+    
+# root.mainloop()
 
+##
+## Using the grid geometry manager 
+##
+
+# For organizing widgits in 2 dimensions, the Grid Geometry Manager is a better choice
+
+# Pros
+# Easy to organize widgets in two dimensions
+# Grid organizational style typical in modern GUI layouts
+
+# Cons
+# Slightly more involved than pack Geometry Manager 
+
+# Planning your GUI:
+# Plan how you'll lay them out ahead of time, like wire framing a web site
+
+# grid_slaves(), grid_configure(), grid_info(), grid_forget()
+
+# from tkinter import *
+# from tkinter import ttk        
+    
+# root = Tk()
+
+# root.rowconfigure(0, weight = 1)
+# root.rowconfigure(1, weight = 3)
+
+# root.columnconfigure(2, weight = 1)
+
+# ttk.Label(root, text = "Yellow",
+#           background = "yellow").grid(row = 0, column = 2, rowspan = 2, stick = 'nsew')
+# ttk.Label(root, text = "Blue",
+#           background = "blue").grid(row = 1, column = 0, columnspan = 2, stick = 'nsew')
+# ttk.Label(root, text = "Green",
+#           background = "green").grid(row = 0, column = 0, stick = 'nsew', padx = 10, pady = 10)
+# ttk.Label(root, text = "Orange",
+#           background = "orange").grid(row = 0, column = 1, stick = 'nsew', ipadx = 25, ipady = 25)
+
+# root.mainloop()
 
 
 
