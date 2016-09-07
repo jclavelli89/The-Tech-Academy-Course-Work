e8806a077d609d7a10c35b8b2df5508c10e660ba
James Clavelli <jclavelli89@gmail.com>
2016-09-06 20:59:33 -0400

    utilized coding comments from Dan

--- Python/PyDrill_scripting_27_idle.py
+++ Python/PyDrill_scripting_27_idle.py
@@ -1,40 +1,21 @@
-import os, os.path, schedule, shutil, time
-import datetime as dt
-from datetime import date,  datetime, timedelta
-
+import os, os.path, shutil, time
 
 
 def daily_copy_script(src_path, dst_path):
     for files in os.listdir(src_path):
-        if files.endswith(".txt") and mtime > ago:
+        if files.endswith(".txt"):
             src_file = os.path.join(src_path, files)
             dst_file = os.path.join(dst_path, files)
-            shutil.move(src_file, dst_path)
-            print("\nFile: {}\nmoved to: {}".format(files, dst_file))
-
-
-
+            fileMod = time.time() - (os.path.getmtime(src_file))
+            _24hrsAgo = time.time() - (24 *60 *60)
+            last24hrs = time.time() - _24hrsAgo
+            if fileMod < last24hrs:
+                shutil.copy(src_file, dst_path)
+                print("\nFile: {}\ncopied to: {}".format(files, dst_file))
 
 if __name__ == '__main__':
     src_path = "C:\Users\James Clavelli\Desktop\A"
     dst_path = "C:\Users\James Clavelli\Desktop\B"
-    now = dt.datetime.now()
-    ago = now-dt.timedelta(hours=24)
-    st = os.stat(src_path)
-    mtime = dt.datetime.fromtimestamp(st.st_mtime)
-    
     daily_copy_script(src_path,dst_path)
 
-
-
-
-
-"""
-utilizing os.stat, we want to add to file_mover a timer utilizing datetime .now
-with > or < symbols and using the read out of a condition provided by os.stat
-related to the new files. In addition, files will need to be copied, not moved
-and the folder will need me to creaet new files to really work it. Should be able to
-compelte this in the 10 - 12 hour this evening
-
-"""
 
